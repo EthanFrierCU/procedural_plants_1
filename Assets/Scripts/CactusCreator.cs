3ce94a18829b787bac0effe59ee300c612869d27
EthanFrier <etfr1107@colorado.edu>
2020-09-16 15:23:09 -0600

    scripted cactus 1 transform and color

---
+++ Assets/Scripts/CactusCreator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CactusCreator : MonoBehaviour
+{
+    public GameObject cactusPrefab1;
+    public int rotationDeg = 20;
+    public int cactusloopsize;
+    public int timer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        cactusloopsize = 180/rotationDeg; 
+        for (int i = 0; i < cactusloopsize; i++) {
+            GameObject cactusSlice = Instantiate(cactusPrefab1);
+            cactusSlice.transform.localEulerAngles = new Vector3(0, i*rotationDeg, 0);
+            cactusSlice.transform.localPosition = new Vector3(0, 1, 0);
+            cactusSlice.GetComponent<Renderer>().material.color = new Color(Random.Range(0.1f,0.3f), Random.Range(0.3f,0.6f), Random.Range(0.1f,0.3f), 100);
+            cactusSlice.transform.parent = transform;
+            cactusSlice.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer = Time.frameCount;
+        foreach (Transform child in transform)
+		{
+		    if ( timer < child.localEulerAngles.y ) {
+				child.gameObject.SetActive(false);
+		    } else {
+				child.gameObject.SetActive(true);
+		    }
+        }
+    }
+}
