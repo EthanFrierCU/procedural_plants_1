d863c7da2e673110bdbbbdf434b64313bfa8996b
EthanFrier <etfr1107@colorado.edu>
2020-09-16 21:41:12 -0600

    scripted cactus grows scales spins

---
+++ Assets/Scripts/cactusScaler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cactusScaler : MonoBehaviour
+{
+    public float seconds = 0;
+    static float t = 0.0f;
+    public float spin = -360f;
+    public float spinScale = 0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        seconds = Time.time;
+
+        transform.localScale = new Vector3(Mathf.Lerp(0.0f,1.0f,t),Mathf.Lerp(0.0f,1.0f,t),Mathf.Lerp(0.0f,1.0f,t));
+        t += 0.3f * Time.deltaTime;
+
+        if (spin <= 0){
+            transform.localEulerAngles = new Vector3(0,spin,0);
+            spin += spinScale;
+        }
+
+    }
+}
